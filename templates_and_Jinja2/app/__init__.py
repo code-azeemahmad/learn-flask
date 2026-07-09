6119ea8fb2edfbf333154e9647440ef8a3e57e7e
code-azeemahmad <azeemahmadd9@gmail.com>
2026-07-09 06:23:45 +0500

    create templates, passing data to templates, write variables and logic in jinja expressions

---
+++ templates_and_Jinja2/app/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+from app.routes.home import home_bp
+from app.routes.books import books_bp
+from app.routes.students import students_bp
+
+
+def create_app():   # Application Factory Pattern
+
+    # Instead of having one fixed app object, we have a factory that can create one whenever it's needed.
+
+    app = Flask(__name__)
+
+    app.config.from_object("app.config.Config")
+
+    app.register_blueprint(home_bp)
+    app.register_blueprint(books_bp)
+    app.register_blueprint(students_bp)
+    
+    return app
