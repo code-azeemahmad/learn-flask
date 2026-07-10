988095598f4456defaf1ad3f3f461b65476ce760
code-azeemahmad <azeemahmadd9@gmail.com>
2026-07-10 15:38:38 +0500

    register auth blueprint

--- authentication/app/__init__.py
+++ authentication/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 from app.routes.home import home_bp
 from app.routes.students import students_bp
+from app.routes.auth import auth_bp
 from app.error_handlers import register_error_handlers
 from app.extensions import db, migrate, bcrypt
 from app.models.student import Student
@@ -18,6 +19,7 @@ def create_app():
 
     app.register_blueprint(home_bp)
     app.register_blueprint(students_bp)
+    app.register_blueprint(auth_bp)
 
     register_error_handlers(app)
 
