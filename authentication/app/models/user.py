81eaebff6d1e178dc1ac873aeeab9a6461ca113e
code-azeemahmad <azeemahmadd9@gmail.com>
2026-07-10 16:48:11 +0500

    add UserMixin to provide default implementations like is_active etc

--- authentication/app/models/user.py
+++ authentication/app/models/user.py
@@ -1,7 +1,11 @@
 from app.extensions import db
+from flask_login import UserMixin
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
+    # UserMixin simply provides these default implementations for you.
+    '''get_id(), is_authenticated, is_active, is_anonymous'''
+
     __tablename__ = "users"
 
     id = db.Column(
@@ -26,6 +30,12 @@ class User(db.Model):
         default="student"
     )
 
+    student = db.relationship(
+        "Student",
+        back_populates="user",
+        uselist=False
+    )
+
 '''
 User
 -----
@@ -41,4 +51,20 @@ Student
 id
 name
 age
+'''
+
+'''
+This is a common design when authentication data and domain data have different responsibilities.
+
+User → Authentication & Authorization
+- email
+- password_hash
+- role
+
+Student → Business/Application data
+- name
+- age
+- courses
+-GPA
+Keeping them separate follows the Single Responsibility Principle and makes it easier to support multiple user types later (e.g., teachers, admins).
 '''
