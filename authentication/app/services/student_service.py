c44014d0ec6d9d5fd67771dc521a328c116c3406
code-azeemahmad <azeemahmadd9@gmail.com>
2026-07-10 12:14:43 +0500

    add business logic and db logic for all other routes in StudentService

--- authentication/app/services/student_service.py
+++ authentication/app/services/student_service.py
@@ -1,32 +1,76 @@
+from sqlalchemy.exc import SQLAlchemyError
+
 from app.extensions import db
 from app.models.student import Student
-from sqlalchemy.exc import SQLAlchemyError
+from werkzeug.exceptions import NotFound
 
 
 class StudentService:
 
     @staticmethod
+    def get_all_students():
+        return Student.query.all()
+
+    @staticmethod
+    def get_student(id):
+
+        student = db.session.get(Student, id)
+
+        if student is None:
+            raise NotFound("Student not found.")
+
+        return student
+
+    @staticmethod
     def create_student(data):
-        """
-        Create a new student and save it to the database.
-
-        Args:
-            data (dict): Validated student data.
-
-        Returns:
-            Student: The newly created student.
-        """
 
         student = Student(**data)
 
         try:
             db.session.add(student)
             db.session.commit()
+
             return student
-    
-        # The service knows exactly what transaction needs rolling back. The error handler doesn't.
-        # let every service be responsible for its own rollback.
 
         except SQLAlchemyError:
             db.session.rollback()
             raise
+
+    @staticmethod
+    def update_student(id, data):
+
+        student = db.session.get(Student, id)
+
+        if student is None:
+            raise NotFound("Student not found.")
+
+        student.name = data["name"]
+        student.age = data["age"]
+        student.email = data["email"]
+
+        try:
+            db.session.commit()
+
+            return student
+
+        except SQLAlchemyError:
+            db.session.rollback()
+            raise
+
+    @staticmethod
+    def delete_student(id):
+
+        student = db.session.get(Student, id)
+
+        if student is None:
+            raise NotFound("Student not found.")
+
+        try:
+            db.session.delete(student)
+            db.session.commit()
+
+        except SQLAlchemyError:
+            db.session.rollback()
+            raise
+
+# Repository layer for db 
