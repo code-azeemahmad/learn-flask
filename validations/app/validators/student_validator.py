ce93a5aa40a73057f63bd7902fd356415d2b72ca
code-azeemahmad <azeemahmadd9@gmail.com>
2026-07-10 07:20:41 +0500

    create a helper method to handle multiple validation messages related to a single field and append it to errors dictionary following DRY principle

--- validations/app/validators/student_validator.py
+++ validations/app/validators/student_validator.py
@@ -3,8 +3,23 @@
 
 class StudentValidator:
 
+    '''DRY Principle
+    A field can have multiple validation messages
+    Append errors.
+    {
+        "name": [
+            "Name is required.",
+            "Minimum length is 3."
+        ]
+    }'''
+
+    @staticmethod
+    def add_error(errors, field, message):
+        errors.setdefault(field, []).append(message)    
+
     @staticmethod   # It doesn't store any state. It only processes the provided input.
     def validate(data):
+
         errors = {}
 
         if not data:
@@ -16,35 +31,53 @@ class StudentValidator:
         # validate name
         name = data.get("name")
         if not name or not name.strip():
-            errors["name"] = [
-                "Name is required."
-            ]
+            StudentValidator.add_error(
+                errors,
+                "name",
+                "name is required"
+            )
+        if len(name) > 16:
+            StudentValidator.add_error(
+                errors,
+                "name",
+                "name should have required length (<16 char)"
+            )
 
         # validate age
         age = data.get("age")
         if age is None:
-            errors["age"] = [
-                "Age is required."
-            ]       
+            StudentValidator.add_error(
+                errors,
+                "age",
+                "age is required"
+            )    
         elif not isinstance(age, int):
-            errors["age"] = [
-                "Age must be an integer."
-            ]
+            StudentValidator.add_error(
+                errors,
+                "age",
+                "age is a number"
+            )   
         elif age < 0:
-            errors["age"] = [
-                "Age cannot be negative."
-            ]
+            StudentValidator.add_error(
+                errors,
+                "age",
+                "age should be positive"
+            )   
 
         # validate email
         email = data.get("email")
         if not email or not email.strip():
-            errors["email"] = [
-                "Email is required."
-            ]
+            StudentValidator.add_error(
+                errors,
+                "email",
+                "email is required"
+            )   
         elif "@" not in email:
-            errors["email"] = [
-                "Invalid email address."
-            ]
+            StudentValidator.add_error(
+                errors,
+                "email",
+                "email should contain @example.com"
+            )   
 
         return errors
 
