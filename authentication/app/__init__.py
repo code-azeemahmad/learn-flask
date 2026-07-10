8ccba49496d09b897788573c042e20a97fcdc4ed
code-azeemahmad <azeemahmadd9@gmail.com>
2026-07-10 14:44:05 +0500

    install and initialize flask_bcrypt

--- authentication/app/__init__.py
+++ authentication/app/__init__.py
@@ -3,8 +3,8 @@ from flask import Flask
 from app.routes.home import home_bp
 from app.routes.students import students_bp
 from app.error_handlers import register_error_handlers
-from app.extensions import db, migrate
-from app.models.student import Student  # No attendance → no migration
+from app.extensions import db, migrate, bcrypt
+from app.models.student import Student
 
 def create_app():
 
@@ -20,15 +20,7 @@ def create_app():
     app.register_blueprint(students_bp)
 
     register_error_handlers(app)
+
+    bcrypt.init_app(app)
     
     return app
-
-
-''' testing
-(.venv) C:\Drives\F_Projects\flask\learn-flask\db_integration>python run.py
-✅ Connected to PostgreSQL!
- * Serving Flask app 'app'
- * Debug mode: on
-WARNING: This is a development server. Do not use it in a production deployment. Use a production WSGI server instead.
- * Running on http://127.0.0.1:5000
-'''
